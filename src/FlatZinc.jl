08129e9332fb584f2a08080171e5e2ca2172ea86
Hans Würfel <git@wuerfel.io>
2021-07-07 19:50:47 +0200

    add lexer

--- src/FlatZinc.jl
+++ src/FlatZinc.jl
@@ -1,5 +1,226 @@
 module FlatZinc
 
-# Write your package code here.
+export Lexer, TokenType, Token, tokenize
+
+@enum TokenType begin
+    PARENTHESES_L
+    PARENTHESES_R
+    BRACKET_L
+    BRACKET_R
+    BRACE_L
+    BRACE_R
+    SEMICOLON
+    COLON
+    COMMA
+    DOUBLECOLON
+    DOTDOT
+    EQUALSIGN
+    NEWLINE
+    KEYWORD
+    BASIC_PAR_TYPE
+    BOOL_LITERAL
+    INT_LITERAL
+    FLOAT_LITERAL
+    IDENTIFIER
+end
+
+const KEYWORDS = [
+    "predicate",
+    "var",
+    "constraint",
+    "of",
+    "in",
+    "array",
+    "solve",
+    "set",
+    "satisfy",
+    "minimize",
+    "maximize"
+]
+
+const TYPES = ["bool", "int", "float"]
+
+struct Token
+    type::TokenType
+    lexme::String
+end
+Token(t::TokenType) = Token(t, "")
+
+function Base.show(io::IO, t::Token)
+    print(io, t.type)
+    isempty(t.lexme) || print(io, " : ", t.lexme)
+end
+
+struct Lexer{IO_t <: IO}
+    io::IO_t
+end
+Lexer(s::String) = Lexer(IOBuffer(s))
+
+function Base.collect(lex::Lexer)
+    a = Token[]
+    for token in lex
+        push!(a, token)
+    end
+    return a
+end
+
+tokenize(s) = collect(Lexer(s))
+
+function Base.iterate(l::Lexer)
+    seekstart(l.io)
+    iterate(l, l.io)
+end
+
+function Base.iterate(l::Lexer, io)
+    if eof(l.io)
+        return nothing
+    end
+
+    # skip whitespaces
+    c = read(l.io, Char)
+    while c ∈ [' ', '\t']
+        c = read(l.io, Char)
+    end
+
+    # check for magic charactes
+    if c == '('
+        return (Token(PARENTHESES_L), io)
+    elseif c == ')'
+        return (Token(PARENTHESES_R), io)
+    elseif c == '['
+        return (Token(BRACKET_L), io)
+    elseif c == ']'
+        return (Token(BRACKET_R), io)
+    elseif c == '{'
+        return (Token(BRACE_L), io)
+    elseif c == '}'
+        return (Token(BRACE_R), io)
+    elseif c == ';'
+        return (Token(SEMICOLON), io)
+    elseif c == ':'
+        if peek(1, io) == ':'
+            skip(io, 1)
+            return (Token(DOUBLECOLON), io)
+        else
+            return (Token(COLON), io)
+        end
+    elseif c == ','
+        return (Token(COMMA), io)
+    elseif c == '='
+        return (Token(EQUALSIGN), io)
+    elseif c == '\n'
+        return (Token(NEWLINE), io)
+    elseif c == '.'
+        read_single(io, '.'; needsmatch=true)
+        return (Token(DOTDOT), io)
+    end
+
+    # check for word
+    if inany(c, 'a':'z', 'A':'Z')
+        word = string(c)
+        word *= read_while(io, 'a':'z', 'A':'Z', '0':'9', '_')
+
+        if word ∈ KEYWORDS
+            return (Token(KEYWORD, word), io)
+        elseif word ∈ TYPES
+            return (Token(BASIC_PAR_TYPE, word), io)
+        elseif word ∈ ["true", "false"]
+            return (Token(BOOL_LITERAL, word), io)
+        else
+            return (Token(IDENTIFIER, word), io)
+        end
+    end
+
+    # number literal
+    word = string(c)
+    if c == '-'
+        c = read(l.io, Char)
+        @assert c ∈ '0':'9' "Found minus without following number!"
+        word *= c
+    end
+
+    if inany(c, '0':'9')
+        if c=='0' && peek(1, io) ∈ 'x' # hex literal
+            word *= read(io, Char)
+            word *= read_while(io, '0':'9', 'a':'f', 'A':'F'; needsmatch=true)
+            return (Token(INT_LITERAL, word), io)
+        elseif c=='0' && peek(1, io) ∈ 'o' #?? literal
+            word *= read(io, Char)
+            word *= read_while(io, '0':'7'; needsmatch=true)
+            peek(1, io) ∉ '8':'9' || error("oh boy that doesnt fit the oct literal")
+            return (Token(INT_LITERAL, word), io)
+        else
+            word *= read_while(io, '0':'9') # read rest of number
+            if peek(1, io) ∈ ('.', 'e', 'E') && inany(peek(2, io), '0':'9', '+', '-')# ouuhh it's a float
+                word *= read_single(io, '.')
+                word *= read_while(io, '0':'9', needsmatch=true)
+                word *= read_single(io, ('e','E'))
+                if word[end] ∈ ('e','E')
+                    word *= read_single(io, ('+','-'))
+                    word *= read_while(io, '0':'9')
+                end
+                return (Token(FLOAT_LITERAL, word), io)
+            else # this is an iteger!
+                return (Token(INT_LITERAL, word), io)
+            end
+        end #
+    end
+
+    error("No token found: ", context(io))
+end
+
+function read_while(io, collections...; needsmatch=false)
+    str = ""
+    try
+        c = read(io, Char)
+        while inany(c, collections...)
+            str *= c
+            c = read(io, Char)
+        end
+        skip(io, -1) # go one back
+    catch e
+        e isa EOFError || rethrow(e)
+    end
+    needsmatch && isempty(str) && error("Nothing matches $collections here: $(context(io))")
+    return str
+end
+
+function read_single(io, collections...; needsmatch=false)
+    eof(io) && !needsmatch && return ""
+    c = read(io, Char)
+    if inany(c, collections...)
+        return string(c)
+    else
+        skip(io, -1)
+        needsmatch && error("Nothing matches $collections here: $(context(io))")
+        return ""
+    end
+end
+
+function inany(e, collections...)
+    for c in collections
+        e ∈ c && return true
+    end
+    return false
+end
+
+function peek(i::Int, io, T=Char)
+    pos = position(io)
+    skip(io, i-1)
+    r = read(io, T)
+    seek(io, pos)
+    return r
+end
+
+function context(io; before=10, after=10)
+    pos = position(io)
+    skip(io, -before)
+    str = ""
+    for i in 1:(before+after)
+        str *= read(io, Char)
+    end
+    seek(io, pos)
+    return str
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,80 @@ using FlatZinc
 using Test
 
 @testset "FlatZinc.jl" begin
-    # Write your tests here.
+    @testest "inany" begin
+        using FlatZinc: inany
+        @test !inany('a', 'b':'d', 'A':'Z')
+        @test inany('a', 'a':'d', 'A':'Z')
+        @test inany('B', 'a':'d', 'A':'Z')
+    end
+
+    @testest "read while" begin
+        using FlatZinc: read_while
+        iob = IOBuffer("123abc#12")
+        @test read_while(iob, '1':'3', 'a':'z') == "123abc"
+        @test read(iob, Char) == '#'
+
+        iob = IOBuffer("123abc#12")
+        @test read_while(iob, 'a':'z') == ""
+        @test read_while(iob, '1':'3', 'a':'z', '#') == "123abc#12"
+        @test eof(iob)
+        iob = IOBuffer("123abc#12!")
+        @test read_while(iob, '1':'3', 'a':'z', '#') == "123abc#12"
+    end
+
+    @testset "peek2" begin
+        io = IOBuffer("12345")
+        pos = position(io)
+        @test peek(1, io) == '1'
+        @test peek(2, io) == '2'
+        @test position(io) == pos
+    end
+
+    @testset "integer parsing" begin
+        tok = tokenize("0x23fA -0o123456 9 -15;")
+        @test tok[1].lexme == "0x23fA"
+        @test tok[2].lexme == "-0o123456"
+        @test tok[3].lexme == "9"
+        @test tok[4].lexme == "-15"
+    end
+
+    @testset "float parsing" begin
+        tok = tokenize("1.0 -2.3 -1e-15 8e12 -1.23E+4;")
+        @test tok[1].lexme == "1.0"
+        @test tok[2].lexme == "-2.3"
+        @test tok[3].lexme == "-1e-15"
+        @test tok[4].lexme == "8e12"
+        @test tok[5].lexme == "-1.23E+4"
+    end
+
+    @testset "tokenize files" begin
+        small_file = joinpath(@__DIR__,"files", "queens3_4.fzn")
+        big_file = joinpath(@__DIR__,"files", "2018_test-scheduling_t100m10r3-2.fzn")
+        for file in [small_file, big_file]
+            tokens = open(file) do io
+                tokenize(io)
+            end
+            i, b, f = 0, 0, 0
+            for t in tokens
+                if t.type == FlatZinc.INT_LITERAL
+                    parse(Int, t.lexme)
+                    i += 1
+                end
+                if t.type == FlatZinc.BOOL_LITERAL
+                    parse(Bool, t.lexme)
+                    b += 1
+                end
+                if t.type == FlatZinc.FLOAT_LITERAL
+                    parse(Float64, t.lexme)
+                    f += 1
+                end
+            end
+            println("Parsed $i integers, $b bools and $f floats correctly")
+        end
+
+        tokens = open(file) do io
+            tokenize(io)
+        end
+        idents = filter(t -> t.type == FlatZinc.IDENTIFIER, tokens)
+    end
 end
